391d4e57aaec56984ab6bd71815b49c960f8d28c
anafdal <adalipi@purdue.edu>
2020-09-29 18:05:21 -0400

    Testing

---
+++ Data Visualization Test/Assets/GetData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetData : MonoBehaviour
+{
+
+    public static string data;
+
+   void Start()
+    {
+        data = "Click For Data";
+        
+    }
+    private void OnMouseDown()
+    {
+
+        //Debug.Log(this.name);
+     
+
+         data = this.name;
+            
+    }
+    private void OnMouseUp()
+    {
+
+        //Debug.Log(this.name);
+
+
+        data = "Click For Data";
+
+    }
+
+
+}
