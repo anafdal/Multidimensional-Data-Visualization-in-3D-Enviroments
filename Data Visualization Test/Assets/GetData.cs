0375f4787d68b9327a6d297cd53ab3d97a9fa212
anafdal <adalipi@purdue.edu>
2020-09-29 19:01:17 -0400

    Testing

--- Data Visualization Test/Assets/GetData.cs
+++ Data Visualization Test/Assets/GetData.cs
@@ -3,32 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class GetData : MonoBehaviour
 {
 
-    public static string data;
+    public static string data;//textual information
+    
 
-   void Start()
+    private Material m_Material;//material to get shader info from
+    private Color startColor;//save original color
+
+    void Start()
     {
-        data = "Click For Data";
-        
+        //data = "Click For Data";
+       
+       //Fetch the Material from the Renderer of the GameObject
+        m_Material = GetComponent<Renderer>().material;
+        UItext.visibility = 0;
+       
     }
     private void OnMouseDown()
     {
 
         //Debug.Log(this.name);
-     
+        UItext.visibility = 1;
 
          data = this.name;
-            
+
+      
+
+        startColor = this.m_Material.color;
+         this.m_Material.color = Color.yellow;
+       
+
+
     }
     private void OnMouseUp()
     {
 
         //Debug.Log(this.name);
+        //data = "Click For Data";
+        UItext.visibility = 0;
 
-
-        data = "Click For Data";
+        this.m_Material.color = startColor;
 
     }
 
--- Data Visualization Test/Assets/UItext.cs
+++ Data Visualization Test/Assets/UItext.cs
@@ -7,16 +7,37 @@ public class UItext : MonoBehaviour
 {
 
     public Text data;
+    public Canvas info;
+    public static int visibility;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        //info = GetComponent<Canvas>();
+        info.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         data.text = GetData.data;
+
+
+        if (visibility == 1)
+        {
+            info.enabled = true;
+        }
+        else if(visibility==0)
+        {
+            info.enabled = false;
+        }
+        else
+        {
+            Debug.Log("There has been an error");
+        }
+
+
+
     }
+
+ 
 }
