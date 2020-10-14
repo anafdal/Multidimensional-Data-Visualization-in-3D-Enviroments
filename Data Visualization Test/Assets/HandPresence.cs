058e6287772588a800d40c6b1bc3ea7539b1cc31
anafdal <adalipi@purdue.edu>
2020-10-13 20:31:08 -0400

    testing

---
+++ Data Visualization Test/Assets/HandPresence.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public InputDeviceCharacteristics controllerCharacteristics;
+    private InputDevice targetDevice;
+    public List<GameObject> controllerPrefabs;
+    private GameObject spawnedController;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+        List<InputDevice> devices = new List<InputDevice>();
+        //InputDevices.GetDevices(devices);// containes everything we use
+        //InputDeviceCharacteristics rightController = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics,devices);
+
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);//name must match in prefab with VR device
+            if (prefab)
+            {
+                spawnedController=Instantiate(prefab, transform);
+                spawnedController.transform.localScale = new Vector3(50, 50, 50);
+
+               
+
+                Debug.Log("Found " + targetDevice.name+ "  "+spawnedController.transform.localScale.magnitude);
+            }
+            else
+            {
+                Debug.LogError("Did not find corresponding controller model: "+ targetDevice.name);
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryValue);//press primary button B on right controller and get value
+
+        if (primaryValue)
+        {
+            Debug.Log("Pressing Primary Button");
+        }
+    }
+}
