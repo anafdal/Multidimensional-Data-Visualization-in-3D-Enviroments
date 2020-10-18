c6cec50ddbb644c7a21b86bac31856e63e983c46
anafdal <adalipi@purdue.edu>
2020-10-18 18:36:42 -0400

    testing

--- Data Visualization Test/Assets/ContiniousMovement.cs
+++ Data Visualization Test/Assets/ContiniousMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
+
 //code is from Valem
 //moving around
 public class ContiniousMovement : MonoBehaviour
@@ -12,6 +13,7 @@ public class ContiniousMovement : MonoBehaviour
     private CharacterController character;//character controller
     private XRRig rig;//VR rig
 
+    public float additionalHeight = 0.2f;
     public float speed = 50;
 
     // Start is called before the first frame update
@@ -28,12 +30,23 @@ public class ContiniousMovement : MonoBehaviour
         device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
         
     }
+    //character should follow movement
+    void CapsuleFollowHeadset()
+    {
+        character.height = rig.cameraInRigSpaceHeight + additionalHeight;
+        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
+        character.center = new Vector3(capsuleCenter.x, character.height / 2 + character.skinWidth, capsuleCenter.z);
+    }
 
     private void FixedUpdate()
     {
+        CapsuleFollowHeadset();
         Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);//what makes direction follow head movement
         Vector3 direction = headYaw*new Vector3(inputAxis.x, 0, inputAxis.y);
 
         character.Move(direction * Time.deltaTime*speed);
     }
+
+
+  
 }
