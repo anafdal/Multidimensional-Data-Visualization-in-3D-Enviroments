8bad3b2d04276307e40e57a577afc957bf3cc927
anafdal <adalipi@purdue.edu>
2020-11-09 15:48:53 -0500

    Testing

--- Data Visualization Test_AR/Hololens21/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_mono-runtime.cpp
+++
@@ -1,3 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\anafi\Desktop\Lab 1\Visualization_Test\Data Visualization Test_AR\Hololens21\Il2CppOutputProject\IL2CPP\libil2cpp\mono-runtime\il2cpp-callbacks.cpp"
-#include "C:\Users\anafi\Desktop\Lab 1\Visualization_Test\Data Visualization Test_AR\Hololens21\Il2CppOutputProject\IL2CPP\libil2cpp\mono-runtime\il2cpp-mono-support.cpp"
